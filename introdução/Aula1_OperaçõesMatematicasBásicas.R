a4ad3cde2baaf54841b638cc6e4ecd4b7aac48a5
Aulas <Aulas>
2016-08-01 20:29:41 -0300

    Aula 1 - Operações Matemáticas (Correção)

--- introdução/Aula1_OperaçõesMatematicasBásicas.R
+++ introdução/Aula1_OperaçõesMatematicasBásicas.R
@@ -1,3 +1,24 @@
 #Operações básicas
 #Adição
 4 + 5
+
+#Subtração
+4 - 5
+
+#Divisão
+4 / 6
+
+#Multiplicação
+4 * 6
+
+#Exponenciação
+4 ^ 4
+
+#Resto de Divisão
+2 %% 2
+
+#Ordem de parenteses
+2 * (2 + 1)
+
+#Ordem Matemática
+1 + 2 * 2
