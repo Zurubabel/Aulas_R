26c1198688ee67024c6ea8216bdb908d403c0d92
Aulas <Aulas>
2016-08-02 11:39:36 -0300

    Aula 5 - Verificação de tipo de dado

---
+++ introdução/Aula5_VerificarTipoDeDado.R
@@ -0,0 +1,15 @@
+#Aula 5 - Verificação de tipo de dado
+
+#ambiente
+varC <- "5"
+varI <- 5
+varN <- 4.3
+varL <- "TRUE"
+
+#funções is.[tipo de dado]([variável a ser verificada]) -> TRUE OU FALSE
+
+# is.numeric - se é ponto flutuante
+# is.integer - se é inteiro
+# is.logical - se é um Verdadeiro ou Falso
+# is.character - se é texto
+# is.double - se é número de dupla precisão 
