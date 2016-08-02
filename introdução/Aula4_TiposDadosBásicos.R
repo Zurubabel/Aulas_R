3bc4fc3c5a3dad02b8c8696f11af40e2282b562c
Aulas <Aulas>
2016-08-02 11:16:51 -0300

    Aula 4 - Tipos Básicos de Dados

---
+++ introdução/Aula4_TiposDadosBásicos.R
@@ -0,0 +1,27 @@
+#Aula 4 - Tipos básicos de dados
+
+#(Números)
+#Inteiro - Integer
+4
+class(4L) 
+#  Sempre colocar o L na frente do número para o R entender que é um
+#número inteiro.
+
+#Ponto flutuante - Numeric
+4.3
+class(65.999)
+
+#Caractere - character
+"Churros"
+class("Churros")
+
+
+#Lógico - Lógico TRUE ou FALSE / T ou F
+class(TRUE) #logical
+class(FALSE) #logical
+class(t) #function
+class(T) #logical
+class(f) #Não encontrado
+class(F) #logical
+
+#complex (uma aula só pra ele [quando eu também aprender a usar. HUE])
