a9d6869fff71516b6f64217cae488c7e76f55b76
Aulas <Aulas>
2016-08-01 21:41:41 -0300

    Aula 3 - Workspace

---
+++ introdução/Aula3_Workspace.R
@@ -0,0 +1,17 @@
+# Aula 3 - Workspace (Básico)
+
+#Criar variável x
+
+x <- 0
+
+#Criando outra variável
+variavelWorkspace <- "Exemplo"
+
+#Listar os objetos no workspace
+ls()
+
+#Apagar objeto/variável do workspace
+rm([nome da varíável])
+
+#Apagar todos os objetos/variáveis do workspace
+rm(list = ls())
