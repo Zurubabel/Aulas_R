32a889ff1e99473c2799c93fafe669c4ced795a4
Aulas <Aulas>
2016-08-02 14:49:55 -0300

    Aula 7 0 Criando Vetores

---
+++ introdução/Aula7_CriandoVetores.R
@@ -0,0 +1,23 @@
+#Aula 7 - Vetores
+
+seg <- 5
+ter <- 4
+qua <- 1
+qui <- 6
+sex <- 2
+
+#Criando vetores - função c
+
+vitorias <- c(5, 4, 1, 6, 2)
+
+vitorias # Retorna o vertor inteiro
+
+vitorias[2] # Endereço de vetores no R começam pelo 1
+
+x <- 90
+
+x[1] # Todas as variáveis no R são vetores
+
+is.vector(x) # True
+is.vector(90) # True
+is.vector(90[1]) #True
