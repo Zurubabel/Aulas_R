1a5d6a70a12f444f21807f434851266e7c2c978f
Aulas <Aulas>
2016-08-01 21:07:04 -0300

    Aula 2 - Criando variáveis

---
+++ introdução/Aula2_Variaveis.R
@@ -0,0 +1,17 @@
+#Aula 2 - Variávis
+
+#Atribuindo valores / criando variáveis
+
+variavel <- 10
+variavel # valor : 10
+
+# Atribuir outro tipo de valor na variável "variavel"
+variavel <- "Churros"
+variavel
+
+#Resultado de um cálculo
+
+x <- 20 ^ 2
+
+x / 4
+
