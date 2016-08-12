e2ae5f08792b8cc7f407d4bf1b5380571a62dcd6
Aulas <Aulas>
2016-08-12 12:21:17 -0300

    Aula 21 - Factors

---
+++ introdução/Aula21_Factors.R
@@ -0,0 +1,8 @@
+#Aula 21 - Factors
+
+vetorJogadores <- c("BR","BR","BR","BR","ARG","ARG","ARG","PAR","PAR","CHI","BR","BR","BR","BR","ARG","ARG","ARG","PAR","PAR","CHI","BR","BR","BR","BR","ARG","ARG","ARG","PAR","PAR","CHI")
+
+factorJogadores <- factor(vetorJogadores)
+str(factorJogadores )
+
+summary(factorJogadores)
