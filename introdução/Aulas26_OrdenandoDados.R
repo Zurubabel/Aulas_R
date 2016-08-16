02b193baea9ce983116f79b4d9f60f4a482372a2
Aulas <Aulas>
2016-08-16 11:41:14 -0300

    Aula 25 - Ordenando dados

---
+++ introdução/Aulas26_OrdenandoDados.R
@@ -0,0 +1,21 @@
+# Aula 26 - Ordenando dados
+
+#Nomes 
+
+nomes <- c("Hebe Camargo", "Sílvio Santos", "Celso Portioli", "Gugu", "Golias")
+anoNascimento <- c(1929, 1930, 1967, 1959, 1929)
+idade <- c(83, 85, 49, 57, 76)
+sexo <- c("F", "M", "M", "M", "M")
+falecido <- c(TRUE, FALSE, FALSE, FALSE, TRUE)
+
+dfDados <- data.frame(nomes, anoNascimento, idade, sexo, falecido)
+
+dfDadosIdade <- dfDados[order(dfDados$idade),]
+?order
+
+
+dfDados[order(dfDados$idade),] #Idade: menor para maior
+
+dfDados[order(dfDados$idade, decreasing = TRUE),]
+
+
